1f47f38d5c880757ecc6c263e7527f6c79f4a349
Lakshita0911 <lakshitarathod111@gmail.com>
2024-10-11 09:20:46 -0400

    Add files via upload

---
+++ CENTRIGA.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ float f,c;
+ printf("enter the value of f:");
+ scanf("%f",&f);
+ c=(float)5*(f-32)/9;
+ printf("value of c=%f",c);
+ return 0;
+ }
